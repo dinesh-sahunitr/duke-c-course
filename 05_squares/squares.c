f8c4642e6d4a3fd5fa0363294f90d9db16dd478a
Coursera Learner <nobody@nowhere.nul>
2019-10-02 05:15:05 +0000

    5th Done

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,10 +9,8 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
-  if(coord>=offset&&coord<offset+size)
-    return 1;
-  else
     return 0;
+    return (coord>= offset && coord<offset+size);
 }
 
 /*
@@ -22,23 +20,16 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
-  if(coord==offset || coord==offset+size)
-    return 1;
-  else
-    return 0;
+  return 0;
+  return (coord==offset || coord==offset+size); 
 }
-int maximum(int a,int b)
-{
-  if(a>=b)
-    return a;
-  else
-    return b;
-}
+
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int  w = maximum(size1,(x_offset + size2));
+  int w = (size1 > (x_offset+size2))? size1 : (x_offset + size2);
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int  h = maximum(size1,(y_offset + size2));
+  int h = (size1 > (y_offset + size2))? size1 : (y_offset + size2);
   //count from 0 to h. Call the number you count with y
   for(int y=0;y<h;y++){
     //count from 0 to w. Call the number you count with x
@@ -59,7 +50,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-      else if(x<size1&&(y==0||y==size1-1)||y<size1&&(x==0||x==size1-1))
+      else if( (x< size1 && (y==0 || y==size1-1))||(y<size1 && (x==0 || x==size1-1)) )
 	printf("#");
       else
 	printf(" ");
